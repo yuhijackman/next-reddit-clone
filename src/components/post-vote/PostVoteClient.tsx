167cb894c3aa2348feee3acf150983d714b253bc
yuhijackman <yuuhiandlisa@gmail.com>
2023-10-17 21:27:09 +0900

    Vote for posts

---
+++ src/components/post-vote/PostVoteClient.tsx
@@ -0,0 +1,69 @@
+"use client";
+import { usePrevious } from "@mantine/hooks";
+import { VoteType } from "@prisma/client";
+import { FC, useEffect, useState } from "react";
+import { Button } from "../ui/Button";
+import { ArrowBigDown, ArrowBigUp } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { useCustomToast } from "@/hooks/use-custom-toast";
+import { useMutation } from "@tanstack/react-query";
+import { PostVoteRequest } from "@/lib/validators/vote";
+import axios from "axios";
+
+interface PostVoteClientProps {
+  postId: string;
+  initialVotesAmt: number;
+  initialVote?: VoteType | null;
+}
+
+const PostVoteClient: FC<PostVoteClientProps> = ({
+  postId,
+  initialVotesAmt,
+  initialVote
+}) => {
+  const { loginToast } = useCustomToast();
+  const [votesAmt, setVotesAmt] = useState<number>(initialVotesAmt);
+  const [currentVote, setCurrentVote] = useState(initialVote);
+  //   if anything goes wrong in the process of voting
+  const prevVote = usePrevious(currentVote);
+
+  useEffect(() => {
+    setCurrentVote(initialVote);
+  }, [initialVote]);
+
+  const {} = useMutation({
+    mutationFn: async (voteType: VoteType) => {
+      const payload: PostVoteRequest = {
+        postId,
+        voteType
+      };
+      await axios.patch("/api/subreddit/post/vote", payload);
+    }
+  });
+
+  return (
+    <div className="flex sm:flex-col gap-4 sm:gap-0 pr-6 sm:w-20 pb-4 sm:pb-0">
+      <Button size="sm" variant="ghost" aria-label="upvote">
+        <ArrowBigUp
+          className={cn("h-5 w-5 text-zinc-700", {
+            "text-emerald-500 fill-emerald-500": currentVote === "UP"
+          })}
+        ></ArrowBigUp>
+      </Button>
+
+      <p className="text-center py-2 font-medium text-sm text-zinc-900">
+        {votesAmt}
+      </p>
+
+      <Button size="sm" variant="ghost" aria-label="downvote">
+        <ArrowBigDown
+          className={cn("h-5 w-5 text-zinc-700", {
+            "text-red-500 fill-red-500": currentVote === "DOWN"
+          })}
+        ></ArrowBigDown>
+      </Button>
+    </div>
+  );
+};
+
+export default PostVoteClient;
