acf4e97021bf9fd45664409f85ec31dc65e76b97
yuhijackman <yuuhiandlisa@gmail.com>
2023-10-19 20:47:51 +0900

    Post Details

--- src/app/r/[slug]/post/[postId]/page.tsx
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -1,12 +1,22 @@
 import { db } from "@/lib/db";
 import { Post, User, Vote } from "@prisma/client";
 import { notFound } from "next/navigation";
+import { Suspense } from "react";
+import PostVoteServer from "@/components/post-vote/PostVoteServer";
+import { buttonVariants } from "@/components/ui/Button";
+import { ArrowBigDown, ArrowBigUp, Loader2 } from "lucide-react";
+import EditorOutput from "@/components/EditorOutput";
+import { formatTimeToNow } from "@/lib/utils";
 
 interface PageProps {
   params: {
     postId: string;
   };
 }
+
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
 const page = async ({ params }: PageProps) => {
   let post: (Post & { votes: Vote[]; author: User }) | null = null;
 
@@ -21,12 +31,68 @@ const page = async ({ params }: PageProps) => {
   });
 
   if (!post) return notFound();
-  //   7:14:00
+
   return (
     <div>
-      <div className="h-full flex flex-col sm:flex-row items-center sm:items-start justify-between"></div>
+      <div className="h-full flex flex-col sm:flex-row items-center sm:items-start justify-between">
+        <Suspense fallback={<PostVoteShell />}>
+          {/* @ts-expect-error server component */}
+          <PostVoteServer
+            postId={post.id}
+            getData={async () => {
+              return await db.post.findUnique({
+                where: {
+                  id: params.postId
+                },
+                include: {
+                  votes: true
+                }
+              });
+            }}
+          />
+        </Suspense>
+
+        <div className="sm:w-0 w-full flex-1 bg-white p-4 rounded-sm">
+          <p className="max-h-40 mt-1 truncate text-xs text-gray-500">
+            Posted by u/{post.author.username}{" "}
+            {formatTimeToNow(new Date(post.createdAt))}
+          </p>
+          <h1 className="text-xl font-semibold py-2 leading-6 text-gray-900">
+            {post.title}
+          </h1>
+
+          <EditorOutput content={post.content} />
+          {/* <Suspense
+            fallback={
+              <Loader2 className="h-5 w-5 animate-spin text-zinc-500" />
+            }
+          >
+            <CommentsSection postId={post.id} />
+          </Suspense> */}
+        </div>
+      </div>
     </div>
   );
 };
 
+function PostVoteShell() {
+  return (
+    <div className="flex items-center flex-col pr-6 w-20">
+      {/* upvote */}
+      <div className={buttonVariants({ variant: "ghost" })}>
+        <ArrowBigUp className="h-5 w-5 text-zinc-700" />
+      </div>
+
+      {/* score */}
+      <div className="text-center py-2 font-medium text-sm text-zinc-900">
+        <Loader2 className="h-3 w-3 animate-spin" />
+      </div>
+
+      {/* downvote */}
+      <div className={buttonVariants({ variant: "ghost" })}>
+        <ArrowBigDown className="h-5 w-5 text-zinc-700" />
+      </div>
+    </div>
+  );
+}
 export default page;
